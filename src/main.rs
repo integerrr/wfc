3df20885010f60946a59a45a5b6674b58596c788
integer <7279624+integerrr@users.noreply.github.com>
2023-04-30 00:20:02 +0100

    more cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,16 @@ use lesson::LessonListing;
 use user::User;
 
 
-fn login(&mut users: Vec<User>, &mut lessons: Vec<LessonListing>) {
+fn login(mut users: &mut Vec<User>, mut lessons: &mut Vec<LessonListing>) {
     let mut username = String::new();
+    println!("*******************************************");
     println!("Login username:");
     io::stdin().read_line(&mut username)?;
     
 }
 
 fn print_main_menu() {
+    println!("*******************************************");
     println!("Hello, world!");
     
     println!("Welcome to WFC booking system");
@@ -39,7 +41,7 @@ fn main() -> io::Result<()> {
         io::stdin().read_line(&mut menu_option)?;
 
         match menu_option.trim().parse::<u8>() {
-            Ok(1) => login(users, lessons),
+            Ok(1) => login(&mut users, &mut lessons),
             Ok(2) => break,
             Ok(_) => println!("invalid"),
             Err(e) => println!("Error: {e}"),
